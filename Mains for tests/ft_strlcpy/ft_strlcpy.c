aea98e981392cd932a13d18e057cac364c55e6fd
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-24 10:15:39 +0100

    Update ft_strlcpy.c

--- Mains for tests/ft_strlcpy/ft_strlcpy.c
+++ Mains for tests/ft_strlcpy/ft_strlcpy.c
@@ -1,23 +1,3 @@
-#include <stddef.h>
-#include <string.h>
-
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	size_t	src_len;
-
-	src_len = strlen(src);
-	if (size > 0)
-	{
-		while (*src && size > 1)
-		{
-			*dest++ = *src++;
-			size--;
-		}
-		*dest = '\0';
-	}
-	return (src_len);
-}
-
 #include <stdio.h>
 
 int main(void)
