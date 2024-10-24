fe1d59948c24de9e59cd31294b5effa46e0dda13
Adão Gonçalves <adao-goncalves@outlook.com>
2024-10-24 10:16:15 +0100

    Update ft_strrchr.c

--- Mains for tests/ft_strrchr/ft_strrchr.c
+++ Mains for tests/ft_strrchr/ft_strrchr.c
@@ -1,21 +1,3 @@
-#include <stddef.h>
-
-char	*ft_strrchr(const char *s, int c)
-{
-	const char	*last;
-
-	last = NULL;
-	while (*s != '\0')
-	{
-		if (*s == (char)c)
-			last = s;
-		s++;
-	}
-	if ((char)c == '\0')
-		return ((char *)s);
-	return ((char *) last);
-}
-
 #include <stdio.h>
 
 int	main(void)
